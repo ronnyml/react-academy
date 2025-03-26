3913be044555689a1b2230e73eb064b4e4073bc3
Ronny Yabar <ronnycontacto@gmail.com>
2025-03-26 01:45:16 -0500

    Include settings locale strings.

--- src/i18n/i18n.ts
+++ src/i18n/i18n.ts
@@ -1,9 +1,9 @@
-import i18n from 'i18next';
-import { initReactI18next } from 'react-i18next';
-//import LanguageDetector from 'i18next-browser-languagedetector';
+import i18n from "i18next";
+import { initReactI18next } from "react-i18next";
+import LanguageDetector from "i18next-browser-languagedetector";
 
-import translationES from './locales/es.json';
-import translationEN from './locales/en.json';
+import translationES from "./locales/es.json";
+import translationEN from "./locales/en.json";
 
 const resources = {
   en: {
@@ -15,16 +15,16 @@ const resources = {
 };
 
 i18n
-  //.use(LanguageDetector)
+  .use(LanguageDetector)
   .use(initReactI18next)
   .init({
     resources,
-    fallbackLng: 'es',
+    fallbackLng: "es",
     detection: {
-      order: ['localStorage', 'navigator'],
-      caches: ['localStorage']
+      order: ["localStorage", "navigator"],
+      caches: ["localStorage"]
     },
-    debug: process.env.NODE_ENV === 'development',
+    debug: process.env.NODE_ENV === "development",
     interpolation: {
       escapeValue: false
     }
